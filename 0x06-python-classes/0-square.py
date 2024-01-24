79fbe2225f38698b10ea081073ea170d04ccb137
BlessedHillary <nnamanihillaryuchenna@gmail.com>
2024-01-23 20:32:00 -0800

    0-square.py

---
+++ 0x06-python-classes/0-square.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+""" Defines a square"""
+
+
+class Square:
+    """
+    This class represents a square.
+    A square of equal side lengths for creating square objects
+
+
+    Attributes:
+        None
+
+    Methods:
+        None
+    """
+    pass
